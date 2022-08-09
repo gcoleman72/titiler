f1168e7a619036ce6b6c9d3724b982248e2022a3
gcoleman72 <g.coleman@aims.gov.au>
2022-08-09 14:03:51 +1000

    zoom in closer on the viewer

--- src/titiler/application/titiler/application/__init__.py
+++ src/titiler/application/titiler/application/__init__.py
@@ -1,3 +1,3 @@
 """titiler.application"""
 
-__version__ = "0.7.1-aims"
+__version__ = "0.7.1"
--- src/titiler/core/titiler/core/__init__.py
+++ src/titiler/core/titiler/core/__init__.py
@@ -1,6 +1,6 @@
 """titiler.core"""
 
-__version__ = "0.7.1-aims"
+__version__ = "0.7.1"
 
 from . import dependencies, errors, factory, routing, utils  # noqa
 from .factory import (  # noqa
